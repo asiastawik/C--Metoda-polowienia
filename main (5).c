eda9b52e5b7ded2e6378651fed315b979f2374c6
Asia <116681445+asiastawik@users.noreply.github.com>
2024-10-21 17:02:18 +0200

    Update main (5).c

--- main (5).c
+++ main (5).c
@@ -1,38 +1,33 @@
 #include <stdio.h>
+#include <math.h>
+
+double funkcja(double x)
+{
+    return sin(x / 180. * 3.14);
+}
+
 int main()
 {
-    int D[] = {10,9,8,7,6,5,4,3,2,1,0}; //gdy rozmiar tablicy jest okreœlony jako „zmienny” D[N] — nie mo¿na nadawaæ tablicy wartoœci pocz¹tkowych.
-    //Zatem albo trzeba „na sztywno” okreœliæ ten rozmiar (D[11]) albo pozostawiæ to miejsce puste
-    int n = sizeof( D ) / sizeof( int );
-    int X;
-
-    printf("Podaj liczbe od 0 do 10 ktora chcesz wyszukac: ");
-    scanf("%d", &X);
-
-    int A, B, C;
-    A = 0;
-    B = n - 1;
-
-    if (X>B || X<A)
-    printf("Nie znaleziono elementu");
-
-    else
-    {
-        C=(A+B)/2;
-
-        while ( X != D[C])
+    double a, b, c, e;
+    a = 30.;
+    b = 220.;
+    e = 0.001;
+    while (b - a >= e)
         {
-            if ( X<D[C] )
-            A = C+1;
+            c = (b + a) / 2.;
+            if (funkcja(a) * funkcja(c) <= 0)
+            {
+                b = c;
+            }
+            else if (funkcja(b) * funkcja(c) <= 0)
+            {
+                a = c;
+            }
             else
-            B = C-1;
-
-            C=(A+B)/2;
+            {
+                printf("W podanym przedziale nie ma miejsc zerowych ");
+            }
         }
-
-    printf("Odnaleziono szukany element %d pod indeksem D[%d]", X, C);
-    printf("\n A=%d ; C=%d ; B=%d ; X=%d", A, C, B, X); //to tylko do wyświetlenia wartosci, aby zobaczyc, czy dziala
-    }
-
-return 0;
+    printf("%f", c);
+    return 0;
 }
